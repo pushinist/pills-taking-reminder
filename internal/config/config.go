991b862776f3e600d81029a40d2bfd3e46f09c1c
Tsvetkov Maxim <pushiniiist@gmail.com>
2025-03-09 19:25:22 +0200

    init commit

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env string `yaml:"env" env-required:"true"`
+	HTTPServer
+	DB
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"30s"`
+}
+
+type DB struct {
+	Host     string `yaml:"host" env-default:"localhost"`
+	Port     string `yaml:"port" env-default:"5432"`
+	Username string `yaml:"username" env-default:"postgres"`
+	Password string `yaml:"password" env-default:"postgres"`
+	Name     string `yaml:"name"`
+}
+
+func MustLoad() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH environment variable not set")
+	}
+
+	if _, err = os.Stat(configPath); err != nil {
+		log.Fatalf("Config file %s does not exist", configPath)
+	}
+
+	var cfg Config
+
+	if err = cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Error reading config: %v", err)
+	}
+
+	return &cfg
+
+}
