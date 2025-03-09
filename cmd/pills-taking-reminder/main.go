03dfe31d1267acd27498ba92999fe2d1fc094ea3
Tsvetkov Maxim <pushiniiist@gmail.com>
2025-03-09 19:35:51 +0200

    add logger

--- cmd/pills-taking-reminder/main.go
+++ cmd/pills-taking-reminder/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"log/slog"
+	"pills-taking-reminder/internal/config"
+	"pills-taking-reminder/pkg/logger"
+)
+
 func main() {
+	cfg := config.MustLoad()
+
+	log := logger.SetupLogger(cfg.Env)
+	log.Info("logger initialized, starting pills-taking-reminder", slog.String("env", cfg.Env))
+
 }
