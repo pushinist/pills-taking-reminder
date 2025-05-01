be3b326b551c19e1bcba233c0be85530b7cb6d7d
Tsvetkov Maxim <pushiniiist@gmail.com>
2025-05-01 19:05:23 +0300

    repository layer

---
+++ internal/domain/repository/schedule_repository.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"pills-taking-reminder/internal/domain/entities"
+)
+
+var (
+	ErrAlreadyExists = errors.New("schedule already exists")
+	ErrNotFound      = errors.New("schedule was not found")
+)
+
+type ScheduleRepository interface {
+	Create(ctx context.Context, schedule *entities.Schedule) (int64, error)
+	GetByID(ctx context.Context, userID, scheduleID int64) (*entities.Schedule, error)
+	GetALLByUserID(ctx context.Context, userID int64) ([]int64, error)
+	GetNextTakings(ctx context.Context, userID int64, interval string) ([]entities.Taking, error)
+}
