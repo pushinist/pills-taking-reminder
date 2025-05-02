18eeb828841c1232ca167c34319fbeb96065d535
Tsvetkov Maxim <pushiniiist@gmail.com>
2025-05-02 12:06:48 +0300

    edit entry point just to make it work lol

--- cmd/pills-taking-reminder/main.go
+++ cmd/pills-taking-reminder/main.go
@@ -3,77 +3,78 @@ package main
 import (
 	"context"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"pills-taking-reminder/internal/config"
-	grpcServer "pills-taking-reminder/internal/grpc/server"
-	"pills-taking-reminder/internal/server"
-	"pills-taking-reminder/internal/storage/pg"
-	"pills-taking-reminder/pkg/logger"
+	"pills-taking-reminder/internal/infrastructure/container"
 	"sync"
 	"syscall"
+	"time"
 
-	s "pills-taking-reminder/internal/service"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	log := logger.SetupLogger(cfg.Env)
-	log.Info("logger initialized, starting pills-taking-reminder", slog.String("env", cfg.Env))
-
-	log.Info("", slog.Any("config", cfg))
-
-	db, err := pg.New(cfg.DB, log, cfg.NearTakingInterval)
+	container, err := container.New(cfg)
 	if err != nil {
-		log.Error("failed to initialize storage", slog.String("error", err.Error()))
+		slog.Error("failed to init container",
+			slog.String("error", err.Error()))
 		os.Exit(1)
 	}
-	log.Info("storage initialized")
 
-	service := s.NewService(db, log)
+	log := container.Logger
+	log.Info("pills-taking-reminder starting",
+		slog.String("env", cfg.Env),
+		slog.String("http_address", cfg.HTTPServer.Address),
+		slog.String("grpc_adress", cfg.GRPCServer.Address))
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
 	wg := sync.WaitGroup{}
 
-	httpSrv := server.NewServer(service)
-	httpSrv.RegisterRoutes()
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	container.HTTPHandler.RegisterRoutes(router)
+
+	httpServer := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
 
 	wg.Add(1)
-
 	go func() {
 		defer wg.Done()
-
-		log.Info("starting HTTP Server", slog.String("address", cfg.HTTPServer.Address))
-
-		err = httpSrv.Run(cfg.HTTPServer.Address)
-		if err != nil {
+		log.Info("starting http server", slog.String("address", cfg.HTTPServer.Address))
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Error("failed to start http server", slog.String("error", err.Error()))
 			cancel()
 		}
 	}()
 
-	grpcSrv := grpcServer.NewGRPCServer(service, log)
-
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-
-		log.Info("starting gRPC Server", slog.String("address", cfg.GRPCServer.Address))
-
-		err = grpcSrv.Run(cfg.GRPCServer.Address)
-		if err != nil {
-			log.Error("failed to start grpc server", slog.String("error", err.Error()))
+		log.Info("starting gRPC server", slog.String("address", cfg.GRPCServer.Address))
+		if err := container.GRPCServer.Run(cfg.GRPCServer.Address); err != nil {
+			log.Error("failed to start gRPC server", slog.String("error", err.Error()))
 			cancel()
 		}
 	}()
 
-	log.Info("both server started")
+	log.Info("both servers have been started successfully")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	select {
 	case <-sigChan:
@@ -84,8 +85,14 @@ func main() {
 
 	log.Info("shutting both servers down...")
 
-	httpSrv.Stop()
-	grpcSrv.Stop()
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		log.Error("failed to graceful shutdown http server", slog.String("error", err.Error()))
+	}
+
+	container.GRPCServer.Stop()
 
 	wg.Wait()
 	log.Info("app down!")
